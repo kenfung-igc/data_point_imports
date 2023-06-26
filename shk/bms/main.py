9be3a4cbebc61708914a530a878bf3b759900053
Kenneth @IGC <kenneth.fung@igcexpert.com>
2023-06-26 17:34:59 +0800

    look up point description and unit

--- shk/bms/main.py
+++ shk/bms/main.py
@@ -28,6 +28,14 @@ def point_type(point):
     return re.sub('(?<!CO)(\d+)', '#', point.BMS_Name.upper())
 
 
+def point_describe(point, field):
+    global point_description
+    try:
+        return point_description.loc[point.asset_group, point.point_type][field][0]
+    except KeyError:
+        return None
+
+
 def asset_seq(point):
     try:
         return re.search(f'{point.asset_type}-?(?:T\d)?-?(?:L\d\d|RF)?-?(?:Z\d)?-?(?:\w\w\w?)?-?(\d+)', point.reference).group(1)
@@ -114,12 +122,13 @@ def load_points(csv_filename, train=False, feel_lucky=False):
     points['reference'] = (points['BMS_Item Reference'].apply(lambda x: x.split('/')[-1])).str.upper()
     points['suffix'] = points.apply(suffix, axis=1)
     points['reference'] = points['reference'].str.cat(points['suffix'], sep='.').str.rstrip('.')
+    points['point_type'] = points.apply(point_type, axis=1)
 
     if feel_lucky:
         points['asset_type'] = points.apply(asset_type, axis=1)
     if train:
         points = points[~points['asset_type'].isna()]
-    print(f'Loaded {len(points)} points')
+    print(f'\nLoaded {len(points)} points')
     return points
 
 
@@ -154,13 +163,13 @@ def train_asset_type_clf(points, clf=None):
         clf = DecisionTreeClassifier(criterion='entropy', splitter='random', random_state=42)
     clf.fit(X_train, y_train)
     test_df['predicted_asset_type'] = clf.predict(X_test)
-    test_df = test_df.merge(known_points, left_index=True, right_index=True).merge(y_test, left_index=True, right_index=True)
-    print(f"Predicted asset type with {round(precision_score(test_df['actual_asset_type'], test_df['predicted_asset_type'], average='micro'), 3)} precision")
+    test_df = test_df.merge(points, left_index=True, right_index=True).merge(y_test, left_index=True, right_index=True)
+    print(f"\nPredicted asset type with {round(precision_score(test_df['actual_asset_type'], test_df['predicted_asset_type'], average='micro'), 3)} precision")
     test_df['predicted_asset_type_raw'] = test_df['predicted_asset_type']
     test_df['matched'] = test_df.apply(lambda x: bool(re.search(x['predicted_asset_type'], x['reference'])), axis=1)
     test_df.loc[~test_df['matched'], 'predicted_asset_type'] = None
     missed = test_df[test_df['predicted_asset_type'] != test_df['actual_asset_type']]
-    print(f"Misclassified {len(missed)} points:\n {missed[['reference', 'predicted_asset_type_raw']]}")
+    print(f"\nMisclassified {len(missed)} points:\n {missed[['reference', 'predicted_asset_type_raw']]}")
     return clf
 
 
@@ -181,7 +190,7 @@ def predict_asset_type(clf, points):
     points['predicted_asset_type_raw'] = points['predicted_asset_type']
     points.loc[~points['matched'], 'predicted_asset_type'] = None
     points['asset_type'] = points['predicted_asset_type']
-    print(f"Fail to predict asset type for {len(points[~points['matched']].index)} points")
+    print(f"\nFail to predict asset type for {len(points[~points['matched']].index)} points")
     return clf
 
 
@@ -189,14 +198,19 @@ def export_to_csv(points):
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     points.sort_values(by=['asset_group', 'asset_code', 'point_type'])
     points[['asset_group', 'asset_code', 'BMS_Item Reference', 'BMS_Name', 'point_type']].to_csv(f'point_asset_code_{timestamp}.csv')
-    print(f'Exported asset code mappings to point_asset_code_{timestamp}.csv')
+    print(f'\nExported asset code mappings to point_asset_code_{timestamp}.csv')
     point_types = points[['asset_group', 'point_type']].drop_duplicates().sort_values(by=['asset_group', 'point_type'])
-    point_types[~point_types.asset_group.isna()].to_csv(f'point_asset_group_{timestamp}.csv')
-    print(f'Exported asset group mappings to point_asset_group_{timestamp}.csv')
+    point_types = point_types[~point_types.asset_group.isna()]
+    point_types['point_description'] = point_types.apply(point_describe, field='point_description', axis=1)
+    point_types['unit'] = point_types.apply(point_describe, field='BMS_Units', axis=1)
+    point_types.to_csv(f'point_asset_group_{timestamp}.csv')
+    print(f'\nExported asset group mappings to point_asset_group_{timestamp}.csv')
 
 
 if __name__ == '__main__':
     known_points = load_points('points_manually_mapped.csv', train=True, feel_lucky=True)
+    point_description = known_points[['asset_group', 'point_type', 'point_description', 'BMS_Units']].drop_duplicates()
+    point_description.set_index(['asset_group', 'point_type'], inplace=True)
     clf = train_asset_type_clf(known_points)
     known_points = load_points('points.csv', train=True, feel_lucky=True)
     clf = train_asset_type_clf(known_points, clf)
