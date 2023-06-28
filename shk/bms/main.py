1dabb83d8f5245165587a7784bce8a296b07001d
Kenneth @IGC <kenneth.fung@igcexpert.com>
2023-06-28 21:17:42 +0800

    map point description and unit

--- shk/bms/main.py
+++ shk/bms/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.tree import DecisionTreeClassifier
@@ -13,7 +14,8 @@ ASSET_GROUP = pd.read_csv(ASSET_TYPE_FILE, index_col=False).fillna('')
 ASSET_GROUP.set_index('asset_type', inplace=True)
 ASSET_TYPES = ASSET_GROUP.index
 SIMPLE_ASSET_TYPES = ['CHWSH', 'CHDSYS', 'WCC', 'CDWBYPASS', 'TWP', 'CLWT']
-vocab_size = 10000
+POINT_TYPES = ['BI Mapper', 'BO Mapper', 'AV Mapper', 'MSV Mapper', 'AO Mapper', 'AI Mapper', 'BV Mapper', 'JCI Family BACnet Device']
+ALARM_TYPES = ['Analog Alarm', 'Multistate Alarm']
 
 
 def split(vec):
@@ -25,15 +27,7 @@ def standardize(name):
 
 
 def point_type(point):
-    return re.sub('(?<!CO)(\d+)', '#', point.BMS_Name.upper())
-
-
-def point_describe(point, field):
-    global point_description
-    try:
-        return point_description.loc[point.asset_group, point.point_type][field][0]
-    except KeyError:
-        return None
+    return re.sub('(?<!CO)(\d+)', '#', point.bms_name.upper())
 
 
 def asset_seq(point):
@@ -61,18 +55,23 @@ def asset_code(point):
     return None
 
 
-def set_asset_codes(points):
+def map_attributes(points):
     points['asset_seq'] = points.apply(asset_seq, axis=1)
     points['asset_group'] = points.apply(asset_group, axis=1)
     points['asset_code'] = points.apply(asset_code, axis=1)
     points['point_type'] = points.apply(point_type, axis=1)
+    points = points[~points['asset_group'].isna()]
+    mapping = generate_mapping(points)
+    points['point_description'] = points.apply(lambda x: mapping.loc[x.asset_group, x.point_type]['bms_description'][0], axis=1)
+    points['unit'] = points.apply(lambda x: mapping.loc[x.asset_group, x.point_type]['bms_units'][0].lstrip('unitEnumSet.'), axis=1)
+    return points
 
 
 def asset_type(point):
     if hasattr(point, 'asset_type') and point.asset_type:
         return point.asset_type
-    ref = point['BMS_Item Reference']
-    name = point['BMS_Name']
+    ref = point['bms_item_reference']
+    name = point['bms_name']
     for t in ASSET_TYPES:
         if re.search(f'^{t}', name) or re.search(f'\.{t}', ref.split('.')[-1]):
             return t
@@ -107,19 +106,30 @@ def zone(ref):
 
 def suffix(point):
     current_suffix = point.reference.split('.')[-1]
-    alternate = point.BMS_Name.upper()
-    if current_suffix != alternate:
+    alternate = point.bms_name
+    if alternate and current_suffix != alternate:
         return alternate.upper()
     else:
         return ''
 
 
+def generate_mapping(points):
+    points = points[~points['asset_group'].isna()]
+    mapping = points[['asset_group', 'point_type', 'bms_description', 'bms_units']].drop_duplicates()
+    mapping.set_index(['asset_group', 'point_type'], inplace=True)
+    return mapping
+
+
 def load_points(csv_filename, train=False, feel_lucky=False):
     points = pd.read_csv(csv_filename, index_col=False)
-    points['tower'] = points['BMS_Item Reference'].apply(tower)
-    points['floor'] = points['BMS_Item Reference'].apply(floor)
-    points['zone'] = points['BMS_Item Reference'].apply(zone)
-    points['reference'] = (points['BMS_Item Reference'].apply(lambda x: x.split('/')[-1])).str.upper()
+    points.fillna('', inplace=True)
+    points.bms_units.replace(0, '', inplace=True)
+    if 'bms_object_type' in points.columns:
+        points = points[points.apply(lambda x: x.bms_object_type in POINT_TYPES, axis=1)]
+    points['tower'] = points['bms_item_reference'].apply(tower)
+    points['floor'] = points['bms_item_reference'].apply(floor)
+    points['zone'] = points['bms_item_reference'].apply(zone)
+    points['reference'] = (points['bms_item_reference'].apply(lambda x: x.split('/')[-1])).str.upper()
     points['suffix'] = points.apply(suffix, axis=1)
     points['reference'] = points['reference'].str.cat(points['suffix'], sep='.').str.rstrip('.')
     points['point_type'] = points.apply(point_type, axis=1)
@@ -128,9 +138,9 @@ def load_points(csv_filename, train=False, feel_lucky=False):
         points['asset_type'] = points.apply(asset_type, axis=1)
     if train:
         points = points[~points['asset_type'].isna()]
-        print(f'\nFitting model on {len(points)} points')
+        print(f'\nFitting model on {len(points)} points from {csv_filename}')
     else:
-        print(f'\nPredicting {len(points)} points')
+        print(f'\nPredicting {len(points)} points from {csv_filename}')
 
     return points
 
@@ -200,26 +210,22 @@ def predict_asset_type(clf, points):
 def export_to_csv(points):
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     points.sort_values(by=['asset_group', 'asset_code', 'point_type'])
-    points[['asset_group', 'asset_code', 'BMS_Item Reference', 'BMS_Name', 'point_type']].to_csv(f'point_asset_code_{timestamp}.csv')
+    points[['asset_group', 'asset_code', 'bms_item_reference', 'bms_name', 'point_type', 'point_description', 'unit']].to_csv(f'point_asset_code_{timestamp}.csv')
     print(f'\nExported asset code mappings to point_asset_code_{timestamp}.csv')
-    point_types = points[['asset_group', 'point_type']].drop_duplicates().sort_values(by=['asset_group', 'point_type'])
+    point_types = points[['asset_group', 'point_type', 'point_description', 'unit']].drop_duplicates().sort_values(by=['asset_group', 'point_type'])
     point_types = point_types[~point_types.asset_group.isna()]
-    point_types['point_description'] = point_types.apply(point_describe, field='point_description', axis=1)
-    point_types['unit'] = point_types.apply(point_describe, field='BMS_Units', axis=1)
     point_types.to_csv(f'point_asset_group_{timestamp}.csv')
     print(f'\nExported asset group mappings to point_asset_group_{timestamp}.csv')
 
 
 if __name__ == '__main__':
     known_points = load_points('points_manually_mapped.csv', train=True, feel_lucky=True)
-    point_description = known_points[['asset_group', 'point_type', 'point_description', 'BMS_Units']].drop_duplicates()
-    point_description.set_index(['asset_group', 'point_type'], inplace=True)
     clf = train_asset_type_clf(known_points)
     known_points = load_points('points.csv', train=True, feel_lucky=True)
     clf = train_asset_type_clf(known_points, clf)
     points = load_points('points.csv', train=False)
     predict_asset_type(clf, points)
     show_feature_importances(clf, show_count=30)
-    set_asset_codes(points)
+    points = map_attributes(points)
     export_to_csv(points.sort_values(by=['asset_type', 'tower', 'floor', 'zone', 'asset_seq']))
     exit(0)
