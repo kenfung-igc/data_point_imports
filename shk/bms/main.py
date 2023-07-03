0fef51fcbd988c8032932d4cbe99f32de36e97d3
Kenneth @IGC <kenneth.fung@igcexpert.com>
2023-07-03 21:15:31 +0800

    rule-based mapping; reconcile to manually mapped asset code

--- shk/bms/main.py
+++ shk/bms/main.py
@@ -1,10 +1,4 @@
 import pandas as pd
-import numpy as np
-import tensorflow as tf
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from datetime import datetime
 import re
 pd.options.mode.chained_assignment = None
@@ -13,13 +7,10 @@ ASSET_TYPE_FILE = 'asset_groups.csv'
 ASSET_GROUP = pd.read_csv(ASSET_TYPE_FILE, index_col=False).fillna('')
 ASSET_GROUP.set_index('asset_type', inplace=True)
 ASSET_TYPES = ASSET_GROUP.index
-SIMPLE_ASSET_TYPES = ['CHWSH', 'CHDSYS', 'WCC', 'CDWBYPASS', 'CLWT', 'GEN', 'MUWP']
+SIMPLE_ASSET_TYPES = ['CHWSH', 'CHDSYS', 'WCC', 'CLWT', 'GEN', 'MUWP']
 POINT_TYPES = ['BI Mapper', 'BO Mapper', 'AV Mapper', 'MSV Mapper', 'AO Mapper', 'AI Mapper', 'BV Mapper', 'JCI Family BACnet Device']
 ALARM_TYPES = ['Analog Alarm', 'Multistate Alarm']
 
-
-def split(vec):
-    return tf.strings.split(tf.strings.split(vec, sep='.'), sep='/')
 
 
 def standardize(name):
@@ -32,7 +23,7 @@ def point_type(point):
 
 def asset_seq(point):
     try:
-        return re.search(f'{point.asset_type}-?(?:T\d)?-?(?:L\d\d|RF)?-?(?:Z\d)?-?(?:\w\w\w?)?-?(\d+)', point.reference).group(1)
+        return re.findall(f'{point.asset_type}-?(?:T\d)?-?(?:L\d\d|RF)?-?(?:Z\d)?-?(\d+)', point.reference)[-1]
     except (AttributeError, IndexError, TypeError):
         return None
 
@@ -56,38 +47,40 @@ def asset_code(point):
 
 
 def map_attributes(points):
-    points['asset_seq'] = points.apply(asset_seq, axis=1)
-    points['asset_group'] = points.apply(asset_group, axis=1)
-    points['asset_code'] = points.apply(asset_code, axis=1)
-    points['point_type'] = points.apply(point_type, axis=1)
-    points['class'] = points.apply(lambda x: 'Alarm' if re.search('alarm', x.bms_object_type.lower()) else 'Point', axis=1)
+    points.loc[:, 'asset_seq'] = points.apply(asset_seq, axis=1)
+    points.loc[:, 'asset_group'] = points.apply(asset_group, axis=1)
+    points.loc[:, 'asset_code'] = points.apply(asset_code, axis=1)
+    points.loc[:, 'point_type'] = points.apply(point_type, axis=1)
+    points.loc[:, 'class'] = points.apply(lambda x: 'Alarm' if re.search('alarm', x.bms_object_type.lower()) else 'Point', axis=1)
     points_to_map = points[~points['asset_group'].isna()]
     mapping = generate_mapping(points_to_map)
-    points['point_description'] = points_to_map.apply(lambda x: mapping.loc[x.asset_group, x.point_type]['bms_description'][0], axis=1)
-    points['unit'] = points_to_map.apply(lambda x: mapping.loc[x.asset_group, x.point_type]['bms_units'][0].lstrip('unitEnumSet.'), axis=1)
+    points.loc[:, 'point_description'] = points_to_map.apply(lambda x: mapping.loc[x.asset_group, x.point_type]['bms_description'][0], axis=1)
+    points.loc[:, 'unit'] = points_to_map.apply(lambda x: mapping.loc[x.asset_group, x.point_type]['bms_units'][0].lstrip('unitEnumSet.'), axis=1)
     return points
 
 
-def asset_type(point):
+def asset_type_by_ref(ref=None):
+    type_by_ref = None
+    if re.search('FC1.', ref):
+        tokens = ref.split('FC1.')[1].split('.')
+        while tokens and not type_by_ref:
+            token = tokens.pop()
+            type_by_ref = next((a for a in ASSET_TYPES if re.search(f'(^{a}|{a}-?\d+|T\d{a})', token)), None)
+    return type_by_ref
+
+
+def set_asset_type(point):
     if hasattr(point, 'asset_type') and point.asset_type:
         return point.asset_type
     ref = point.bms_item_reference
     name = point.bms_name
     type_by_name = next((a for a in ASSET_TYPES if re.search(f'^{a}', name)), None)
-    type_by_ref = None
-
-    if re.search('FC1.', ref):
-        ref = ref.split('FC1.')[1]
-        tokens = ref.split('.')[::-1]
-        while tokens and not type_by_ref:
-            token = tokens.pop()
-            type_by_ref = next((a for a in ASSET_TYPES if re.search(f'^{a}', token)), None)
-
+    type_by_ref = asset_type_by_ref(ref)
     if type_by_name and type_by_ref:
         if type_by_name == type_by_ref:
             return type_by_name
         else:
-            print(f'Name {name} indicates {type_by_name}. Ref {ref} indicates {type_by_ref}. Which to choose?')
+            print(f'Name {point.bms_name} indicates {type_by_name}. Ref {ref} indicates {type_by_ref}. Which to choose?')
     elif type_by_name:
         return type_by_name
     elif type_by_ref:
@@ -133,7 +126,7 @@ def generate_mapping(points):
     return mapping
 
 
-def load_points(csv_filename, train=False, feel_lucky=False, include_alarms=False):
+def load_points(csv_filename, strict=False, mapping=False, include_alarms=False):
     object_types = POINT_TYPES
     if include_alarms:
         object_types += ALARM_TYPES
@@ -149,15 +142,14 @@ def load_points(csv_filename, train=False, feel_lucky=False, include_alarms=Fals
     points['suffix'] = points.apply(suffix, axis=1)
     points['reference'] = points['reference'].str.cat(points['suffix'], sep='.').str.rstrip('.')
     points['point_type'] = points.apply(point_type, axis=1)
+    points = points[points.apply(lambda x: not re.search('SPARE', x.point_type), axis=1)]
 
-    if feel_lucky:
-        points['asset_type'] = points.apply(asset_type, axis=1)
-    if train:
+    if mapping:
+        points['asset_type'] = points.apply(set_asset_type, axis=1)
+    if strict:
         points = points[~points['asset_type'].isna()]
-        print(f'\nFitting model on {len(points)} points from {csv_filename}')
-    else:
-        print(f'\nPredicting {len(points)} points from {csv_filename}')
-
+        points = points[points['asset_type'] != '']
+    print(f'\nLoaded {len(points)} points from {csv_filename}')
     return points
 
 
@@ -168,65 +160,19 @@ def superlist(*args):
     return list(superset)
 
 
-def vectorize(references_raw, vocabulary=[]):
-    token_pattern = "(?u)\\b[\\w#-]+\\b"
-    references = references_raw.apply(standardize)
-    if len(vocabulary) == 0:
-        vectorizer = TfidfVectorizer(token_pattern=token_pattern, lowercase=False)
-        vocabulary = vectorizer.fit(references).get_feature_names_out()
-        vocabulary = [v for v in vocabulary if not any([re.search(t,v) for t in SIMPLE_ASSET_TYPES])]
-        vocabulary = superlist(vocabulary, SIMPLE_ASSET_TYPES)
-    vectorizer = TfidfVectorizer(token_pattern=token_pattern, vocabulary=vocabulary, lowercase=False)
-    refs = vectorizer.fit_transform(references)
-    refs = pd.DataFrame.sparse.from_spmatrix(refs)
-    refs.columns = vectorizer.get_feature_names_out()
-    return refs
-
-
-def train_asset_type_clf(points, clf=None):
-    X = vectorize(points['reference'])
-    y = points['asset_type']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    test_df = y_test.rename('actual_asset_type').to_frame()
-    if not clf:
-        clf = DecisionTreeClassifier(criterion='entropy', random_state=42)
-    clf.fit(X_train, y_train)
-    test_df['predicted_asset_type'] = clf.predict(X_test)
-    test_df = test_df.merge(points, left_index=True, right_index=True).merge(y_test, left_index=True, right_index=True)
-    print(f"Predicted asset type with {round(accuracy_score(test_df['actual_asset_type'], test_df['predicted_asset_type']), 3)} accuracy")
-    test_df['predicted_asset_type_raw'] = test_df['predicted_asset_type']
-    test_df['matched'] = test_df.apply(lambda x: bool(re.search(f"\.{x['predicted_asset_type']}", x['reference'])), axis=1)
-    test_df.loc[~test_df['matched'], 'predicted_asset_type'] = None
-    missed = test_df[test_df['predicted_asset_type'] != test_df['actual_asset_type']]
-    print(f"Failed to predict asset type for {len(missed)} points:\n {missed[['reference', 'predicted_asset_type_raw']]}")
-    return clf
-
-
-def show_feature_importances(clf, show_count=10):
-    importances = clf.feature_importances_
-    reverse = show_count < 0
-    show_count = min(abs(show_count), len(importances))
-    print(f"\n{'Bottom' if reverse else 'Top'} {show_count} features (out of {len(importances)}):")
-    indices = importances.argsort()[:show_count] if reverse else (-importances).argsort()[:show_count]
-    for i in indices:
-        print(f'{clf.feature_names_in_[i]} ({round(importances[i], 4)})')
-
-
-def predict_asset_type(clf, points):
-    X = vectorize(points['reference'], vocabulary=clf.feature_names_in_)
-    points['predicted_asset_type'] = clf.predict(X)
-    points['matched'] = points.apply(lambda x: bool(re.search(x['predicted_asset_type'], x['reference'])), axis=1)
-    points['predicted_asset_type_raw'] = points['predicted_asset_type']
-    points.loc[~points['matched'], 'predicted_asset_type'] = None
-    points['asset_type'] = points['predicted_asset_type']
-    print(f"\nFailed to predict asset type for {len(points[~points['matched']].index)} points")
-    return clf
+def reconcile(mapping, expected_mapping):
+    OUTPUT = ['reference', 'bms_name', 'expected_asset_code', 'asset_code']
+    mismatch = mapping.merge(expected_mapping, how='inner', on='bms_item_reference')
+    mismatch.rename(columns={'asset_code_x': 'asset_code', 'asset_code_y': 'expected_asset_code', 'bms_name_x': 'bms_name', 'reference_x': 'reference'}, inplace=True)
+    mismatch = mismatch[mismatch['asset_code'] != mismatch['expected_asset_code']][OUTPUT]
+    if len(mismatch):
+        print(f"Mappings unexpected:\n{mismatch[OUTPUT]}")
 
 
 def export_to_csv(points):
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     points.sort_values(by=['class', 'asset_group', 'asset_code', 'point_type'], inplace=True)
-    points[['asset_group', 'asset_code', 'bms_item_reference', 'bms_name', 'point_type', 'point_description', 'unit', 'class']].to_csv(f'point_asset_code_{timestamp}.csv')
+    points[['asset_group', 'asset_code', 'bms_item_reference', 'bms_name', 'point_type', 'point_description', 'bms_object_type', 'unit', 'class']].to_csv(f'point_asset_code_{timestamp}.csv')
     print(f'\nExported asset code mappings to point_asset_code_{timestamp}.csv')
     point_types = points[['asset_group', 'point_type', 'point_description', 'unit', 'class']].drop_duplicates()
     point_types = point_types[~point_types.asset_group.isna()]
@@ -235,13 +181,17 @@ def export_to_csv(points):
 
 
 if __name__ == '__main__':
-    known_points = load_points('points_manually_mapped.csv', train=True, feel_lucky=False)
-    clf = train_asset_type_clf(known_points)
-    known_points = load_points('points.csv', train=True, feel_lucky=True)
-    clf = train_asset_type_clf(known_points, clf)
-    points = load_points('points.csv', train=False, include_alarms=True)
-    predict_asset_type(clf, points)
-    show_feature_importances(clf, show_count=30)
-    points = map_attributes(points)
+    points = load_points('points.csv', mapping=True, include_alarms=True)
+    map_attributes(points)
+    known_points = load_points('points_manually_mapped.csv', strict=True)
+    reconcile(points, known_points)
     export_to_csv(points)
+
+    # known_points = load_points('points_manually_mapped.csv', train=True, feel_lucky=False)
+    # clf = train_asset_type_clf(known_points)
+    # clf = train_asset_type_clf(points[~points['asset_type'].isna()], clf)
+    # points_predicted = predict_asset_type(clf, points[points['asset_type'].isna()])
+    # show_feature_importances(clf, show_count=30)
+    # points_predicted = map_attributes(points_predicted)
+    # export_to_csv(points_predicted)
     exit(0)
